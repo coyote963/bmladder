c4734966c5fcb034270dc61effed93f68d2f0929
coyote963 <coyoteandbird@gmail.com>
2016-09-30 22:25:41 -0400

    adds template tags to display comments easier

--- player/models.py
+++ player/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from clans.models import Clan
 
 class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	user = models.OneToOneField(User, on_delete=models.CASCADE, blank = True, null = True)
 	ingamename = models.CharField(max_length=25)
 	avatar = models.ImageField(upload_to='profile_images', blank=True)
 	clan = models.ForeignKey(Clan, blank=True, null=True)
