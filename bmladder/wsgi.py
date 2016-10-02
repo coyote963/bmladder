60cbae19842daf24cf662bcd7f84b64eb783e648
coyote963 <coyoteandbird@gmail.com>
2016-10-01 22:35:52 -0400

    fixes db issue and ends gunicorn support

--- bmladder/wsgi.py
+++ bmladder/wsgi.py
@@ -1,20 +1,15 @@
 """
-WSGI config for bmladder project.
-
+WSGI config for gettingstarted project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
-from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(application)
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bmladder.settings")
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
