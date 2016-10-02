ff6ed47cedf7ead53c164ac54c8c8bacbd1d2088
coyote963 <coyoteandbird@gmail.com>
2016-10-02 12:34:29 -0400

    fixes db issue

--- bmladder/settings.py
+++ bmladder/settings.py
@@ -108,11 +108,7 @@ WSGI_APPLICATION = 'bmladder.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangodb2',
-        'USER': 'jon',
-        'PASSWORD': 'asdfjkl;',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
