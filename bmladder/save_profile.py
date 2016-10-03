a7df92d33bc5bc180247fa280d31414416bab4c4
coyote963 <coyoteandbird@gmail.com>
2016-10-02 23:52:56 -0400

    delays saving the steam avatar

--- bmladder/save_profile.py
+++ bmladder/save_profile.py
@@ -6,12 +6,9 @@ def save_profile(backend, user, response, *args, **kwargs):
         try:
             user.profile.ingamename = kwargs['uid']
         except AttributeError:
-
-    
             player = Profile(
                 user = user,
                 steamid = kwargs['details']['player']['steamid'],
                 ingamename = kwargs['details']['player']['personaname'],
-                steamimage = kwargs['details']['player']['avatarmedium']
                 )
             player.save()
