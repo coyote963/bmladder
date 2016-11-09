48e83b82398daa2da0a6323fc037450fe07f02ec
coyote963 <coyoteandbird@gmail.com>
2016-11-09 18:40:06 -0500

    changes settings to allow new app

--- bmladder/settings.py
+++ bmladder/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'player',
     'ladder',
     'clans',
-    'leaderboards',
+    'leaderboard',
     'social.apps.django_app.default',
     'easy_thumbnails',
     'django.contrib.sites',
