114d948951487d1987d4a276e05916c828bcd7db
coyote963 <coyoteandbird@gmail.com>
2016-11-15 13:59:35 -0500

    closes cursor

--- leaderboards/views.py
+++ leaderboards/views.py
@@ -3,9 +3,12 @@ from django.db import connection
 # Create your views here.
 def index(request):
 	with connection.cursor() as cursor:
-		cursor.execute("SELECT ingamename FROM player ORDER BY rating DESC;")
-		playerlist = cursor.fetchall()
-		playerlist = map(lambda x: x[0], playerlist)
+		try:
+			cursor.execute("SELECT ingamename FROM player ORDER BY rating DESC;")
+			playerlist = cursor.fetchall()
+			playerlist = map(lambda x: x[0], playerlist)
+		finally:
+			cursor.close()
 	return render(request,
 		'leaderboards/index.html',
 		{'playerlist':playerlist})
