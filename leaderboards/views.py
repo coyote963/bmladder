b2de6c98c88d531aae967942645c937ba542090e
coyote963 <coyoteandbird@gmail.com>
2016-11-15 14:14:28 -0500

    adds ratings column

--- leaderboards/views.py
+++ leaderboards/views.py
@@ -4,9 +4,8 @@ from django.db import connection
 def index(request):
 	with connection.cursor() as cursor:
 		try:
-			cursor.execute("SELECT ingamename FROM player ORDER BY rating DESC;")
+			cursor.execute("SELECT ingamename, rating FROM player ORDER BY rating DESC;")
 			playerlist = cursor.fetchall()
-			playerlist = map(lambda x: x[0], playerlist)
 		finally:
 			cursor.close()
 	return render(request,
