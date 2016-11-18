a2521936a80413a7bafd3ff9a733c6c3d4f86beb
coyote963 <coyoteandbird@gmail.com>
2016-11-18 15:32:22 -0500

    import error fixed

--- leaderboards/views.py
+++ leaderboards/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.db import connection
-import numbpy
+import numpy
 # Create your views here.
 def index(request):
 	with connection.cursor() as cursor:
