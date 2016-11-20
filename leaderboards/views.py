c6ef02c96914e1852ca88d00757abd7cbd2856df
coyote963 <coyoteandbird@gmail.com>
2016-11-20 14:09:33 -0500

    adds weapon use chart

--- leaderboards/views.py
+++ leaderboards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db import connection
 import numpy
+from collections import Counter
 # Create your views here.
 def index(request):
 	with connection.cursor() as cursor:
@@ -25,19 +26,25 @@ def playerview(request, pk):
 		{'matchuplist':matchuplist})
 def graph(request):
 	with connection.cursor() as cursor:
-		cursor.execute("SELECT rating FROM player ORDER BY rating ASC")
+		cursor.execute("SELECT rating FROM player ORDER BY rating ASC;")
 		ratinglist = cursor.fetchall()
 		ratinglist = map((lambda x: x[0]), ratinglist)
 		indices = map((lambda x: str(x)), createrange(ratinglist,20))
-		bins = createfrequency(ratinglist, 20)
+		bins = createfrequency(ratinglist, 120)
+
+		cursor.execute("SELECT weapon FROM matchup ORDER BY dateoccurred DESC LIMIT 1000;")
+		weaponlist = cursor.fetchall()
+		weaponcounts = Counter(weaponlist)
+		weapons, frequency = weaponcounts.keys(), weaponcounts.values()
 
 	return render(request,
 		'leaderboards/graph.html',
 		{'ratinglist':ratinglist,
 		'indices':indices,
 		'bins':bins,
-		'lenindices':len(indices),
-		'lenbins': len(bins)})
+		'weapons':weapons,
+		'frequency':frequency})
+
 def createrange(ratinglist, step):
 	minimum =  (ratinglist[0] / step) * step
 	maximum =  (ratinglist[-1] /step ) *step + step
